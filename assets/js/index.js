577a3a12d6fcec34e42873440c2dcd7dc6cb5b89
duanwu1432 <duanwu@qq.com>
2021-03-15 10:05:17 +0800

    主页功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,19 @@ $(function () {
     // 获取用户信息,调用getUserInof()方法
     getUserInof();
 
+    // 退出登录
+    let layer = layui.layer;
+    $('#btnLogout').on('click', function () {
+        //eg1
+        layer.confirm('确定要退出登录吗？', { icon: 3, title: '提示' }, function (index) {
+            // 清空本地token
+            localStorage.removeItem('token');
+            // 页面跳转
+            location.href = '/login.html';
+            //do something
+            layer.close(index);
+        });
+    });
 });
 
 // 获取用户信息(封装到入口函数的外面)  原因  后面有其他的页面也要使用
@@ -11,9 +24,9 @@ function getUserInof() {
         url: '/my/userinfo',
         type: 'get',
         data: {},
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
         success: (res) => {
             // console.log(res);
             if (res.status !== 0) {
@@ -21,9 +34,10 @@ function getUserInof() {
             }
             // 请求成功 ,渲染头像
             renderAvatar(res.data)
-        }
-    })
-}
+        },
+
+    });
+};
 
 function renderAvatar(user) {
     // 渲染名称
@@ -38,6 +52,6 @@ function renderAvatar(user) {
         // 没有头像隐藏图片头像显示文字头像
         $('.layui-nav-img').hide();
 
-        $('.text-avatar').show().html(name[0].toUpperCase())
-    }
-}
+        $('.text-avatar').show().html(name[0].toUpperCase());
+    };
+};
